639682511490381f1e46566297688d69d886744c
tkgunji <gunji@cns.s.u-tokyo.ac.jp>
2014-02-20 17:50:26 +0900

    update

--- core/main.cc
+++ core/main.cc
@@ -4,27 +4,49 @@
 
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
+void print_err(){
+
+    cerr << "FeeServer name not set" << endl
+         << "set the environment variable FEE_SERVER_NAME (export FEE_SERVER_NAME=trd-fee_00_1_2)" << endl
+         << "or" << endl
+         << "do ./feeserver FEE_SERVER_NAME=trd-fee_00_1_2 " << endl;
+
+}
 
 
-
-int main()
+int main(int argc, char **argv)
 {
 
-    if (!getenv("FEE_SERVER_NAME")) {
-        cerr << "FeeServer name not set" << endl
-             << endl
-             << "set the environment variable FEE_SERVER_NAME to the" << endl
-             << "name of the FeeServer (e.g. trd-fee_00_1_2)" << endl;
-            
-        
+    string name ; 
+
+    if(argc==1){
+        if (!getenv("FEE_SERVER_NAME")) {
+            print_err();
+            exit(-1);
+        }
+        name = getenv("FEE_SERVER_NAME");
+    }else if(argc==2){
+        char tmpname[256];
+        sscanf(argv[1],"FEE_SERVER_NAME=%s", tmpname);
+        name = string(tmpname);        
+        if(name==""){
+            print_err();
+            exit(-1);
+        }
+        setenv("FEE_SERVER_NAME",name.c_str(), 1);
+    }else{
+        print_err();
         exit(-1);
     }
-    string name = getenv("FEE_SERVER_NAME");
 
 
+    
+    
     string libce;
     if (!getenv("FEE_SERVER_LIBCE")) {
         libce = "libControlEngine.so";
